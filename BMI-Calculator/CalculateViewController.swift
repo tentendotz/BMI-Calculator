f28ad41214ec90309b1afec2c808272d916226f3
ttt-net <tenten102matsuyama@gmail.com>
2022-06-15 04:48:16 +0900

    Add feature to use UISlider

--- BMI-Calculator/CalculateViewController.swift
+++ BMI-Calculator/CalculateViewController.swift
@@ -16,13 +16,16 @@ class CalculateViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
     }
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weight)Kg"
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
