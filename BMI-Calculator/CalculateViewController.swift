8b290860fab6cddfe5a96637f84198d5012c4d90
ttt-net <tenten102matsuyama@gmail.com>
2022-06-15 04:37:22 +0900

    Add IBOutlet and IBAction

--- BMI-Calculator/CalculateViewController.swift
+++ BMI-Calculator/CalculateViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class CalculateViewController: UIViewController {
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
 
 }
 
